92a7651076020cb39d939626e9341ccb8ae96890
12378 <123781007@qq.com>
2020-04-02 15:45:38 +0800

    fourth

--- src/main/java/org/csu/mypetstore/MypetstoreApplication.java
+++ src/main/java/org/csu/mypetstore/MypetstoreApplication.java
@@ -1,8 +1,9 @@
 package org.csu.mypetstore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("org.csu.mypetstore.persistence")
 @SpringBootApplication
 public class MypetstoreApplication {
 
